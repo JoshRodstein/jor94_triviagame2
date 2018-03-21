03073de903eb1946dc72999a74b099c575067491
Joshua Rodstein <joshuarodstein@gmail.com>
2018-03-21 01:34:16 -0400

    Implemented High Score Tracking in Firebase

--- app/src/main/java/edu/pitt/cs1699/jor94_triviagame2/Scores.java
+++ app/src/main/java/edu/pitt/cs1699/jor94_triviagame2/Scores.java
@@ -4,6 +4,7 @@ package edu.pitt.cs1699.jor94_triviagame2;
 public class Scores {
     private String Timestamp;
     private String Score;
+    private String uid;
 
 
     public Scores() {
@@ -13,6 +14,13 @@ public class Scores {
     public Scores(String t, String s) {
         this.Timestamp = t;
         this.Score = s;
+        this.uid = null;
+    }
+
+    public Scores(String t, String s, String u) {
+        this.Timestamp = t;
+        this.Score = s;
+        this.uid = u;
     }
 
     public String getTimestamp() {
@@ -21,5 +29,11 @@ public class Scores {
 
     public String getScore() {
         return this.Score;
+    }
+
+    public String getUid() { return uid; }
+
+    public void setUid(String id){
+        this.uid = id;
     }
 }
