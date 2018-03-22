43e09831df6986faaa82b313866309de4761f1be
Josh Rodstein <joshuarodstein@gmail.com>
2018-03-22 13:55:08 -0400

    NEEDS TESTING: load profile photo from Firebase to ImageView

--- app/src/main/java/edu/pitt/cs1699/jor94_triviagame2/StartActivity.java
+++ app/src/main/java/edu/pitt/cs1699/jor94_triviagame2/StartActivity.java
@@ -1,6 +1,9 @@
 package edu.pitt.cs1699.jor94_triviagame2;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +12,22 @@ import android.view.*;
 import android.media.MediaPlayer;
 import android.database.sqlite.*;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.net.URL;
+
 
 public class StartActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth = com.google.firebase.auth
+            .FirebaseAuth.getInstance();
+    ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,35 @@ public class StartActivity extends AppCompatActivity {
                     mp3.start();
                     mp3.setLooping(true);
                 }
+            }
+        });
+
+        loadProfileImage();
+    }
+
+    private void loadProfileImage() {
+        FirebaseDatabase fbdb = FirebaseDatabase.getInstance();
+        DatabaseReference dbref = fbdb.getReference().child(mAuth.getCurrentUser().getUid())
+                .child("photo");
+
+        dbref.addListenerForSingleValueEvent( new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String photoURL = dataSnapshot.getValue().toString();
+                profileImage = (ImageView) findViewById(R.id.ImageView);
+                try {
+                    URL url = new URL(photoURL);
+                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    profileImage.setImageBitmap(bmp);
+                }catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
